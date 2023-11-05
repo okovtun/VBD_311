0c4bd5b09b937d2350eb9317b48e972db89ec459
Clayman <Clayman@DESKTOP-DM8E77U>
2023-11-05 16:36:08 +0300

    Operators

--- DataTypes/DataTypes/main.cpp
+++ DataTypes/DataTypes/main.cpp
@@ -1,8 +1,13 @@
 ﻿#include<iostream>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
 //#define LOGICAL_TYPES
 //#define NUMERIC_TYPES
+//#define VARIABLES
+//#define CONSTANTS
 
 void main()
 {
@@ -36,25 +41,50 @@ void main()
 	cout << DBL_MIN << " ... " << DBL_MAX << endl;
 #endif
 
+#if defined VARIABLES
 	int a = 0;		//Инициализация при объявлении
 	cout << a << endl;
 	double price;	//Объявление переменной
 	price = 0;		//Инициализация после объявления
 	double price_of_coffee;
-	cout << "Введите стоимость кофе: "; 
+	cout << "Введите стоимость кофе: ";
 	cin >> price_of_coffee;	//Инициализация вводом с клавиатуры
 	int number_of_cups;
 	cout << "Введите количество чашек: "; cin >> number_of_cups;
 	double total_price = price_of_coffee * number_of_cups;
 	cout << "Общая стоимость покупки: " << total_price << endl;
+#endif
+
+#ifdef CONSTANTS
+	int speed = 0;
+	const int MAX_SPEED = 250;
+	//Константы всегда называют в заглавном регистре
+	//https://legacy.cplusplus.com/doc/tutorial/constants/
+	//http://www.softelectro.ru/ieee754.html
+	cout << sizeof(5) << endl;
+	cout << typeid(1024).name() << endl;
+	55;		//Числовая константа типа 'int'
+	55.;	//Числовая константа типа 'double'
+	.25;	//Числовая константа типа 'double'
+	.125f;	//Числовая константа типа 'float'
+	55l;	//Числовая константа типа 'long'
+	55ll;	//Числовая константа типа 'long long'
+	55ull;	//Числовая константа типа 'unsigned long long'
+
+
+	cout << typeid(55.f).name() << endl;
+	double price;
+	cout << typeid(price).name() << endl;
+#endif // CONSTANTS
+
 }
 
 /*
 ----------------------------------------------
-Переменная (Variable) - это именованная область памяти, 
+Переменная (Variable) - это именованная область памяти,
 содержимое которой может изменяться В ПРОЦЕССЕ ВЫПОЛНЕНИЯ ПРОГРАММЫ.
 Прежде чем использовать переменную ее нужно люъявить (создать)!!!
-						
+
 						type name;		//синтаксис объявления переменной
 type - тип объявляемой переменной, он определяет три вещи:
 	1. Сколько памяти переменная будет занимать;
---
+++ DataTypes/Operators/main.cpp
@@ -0,0 +1,83 @@
+﻿#include<iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+//#define ARITHMETICAL_OPERATORS	//1) Арифметические операторы
+//#define ASSIGNMENT_OPERATOR		//2) Оператор присваивания
+//#define INCREMENT_DECREMENT		//3) ++/--
+#define COMPOUND_ASSIGNMENTS		//4) Составные присваивания
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	cout << "Hello operators" << endl;
+	3;
+	-3;		//Унарный минус
+	8 - 3;	//Бинарный минус
+	8 * 3;	//Бинарное умножение
+	//*3;		//Оператор '*' может быть только бинарным
+#ifdef ARITHMETICAL_OPERATORS
+			//Unary: +, -;
+//Binary:+, -, *, /, %;
+//cout << 17 / 3 << endl;
+//cout << 17 % 3 << endl;
+	cout << 3. / 17 << endl;
+	cout << 3. % 17 << endl;
+#endif // ARITHMETICAL_OPERATORS
+
+#ifdef ASSIGNMENT_OPERATOR
+	//	l-value = r-value;
+//l-value - переменная слева
+//r-value - выражение справа
+//int a = 2;
+
+//a = b + c * d - e / f;
+
+//a + b + c + d
+
+	int a, b, c;
+
+	a = b = c = 0;
+
+	cout << a << "\t" << b << "\t" << c << endl;
+#endif // ASSIGNMENT_OPERATOR
+
+#ifdef INCREMENT_DECREMENT
+	//Increment (++) - это унарный оператор, который увеличивает значение переменной на 1.
+	//Decrement (--) - это унарный оператор, который уменьшает значение переменной на 1.
+	int i = 0;
+	++i;	//Prefix increment
+	i++;	//Postfix increment
+	--i;	//Prefix decrement
+	i--;	//Postfix decrement
+
+	int j = ++i;
+	cout << i << endl;
+	cout << j << endl;
+#endif // INCREMENT_DECREMENT
+
+#ifdef COMPOUND_ASSIGNMENTS
+	/*
+	------------------------------------
+	a = a + b;		a += b;
+	a = a - b;		a -= b;
+	a = a * b;		a *= b;
+	a = a / b;		a /= b;
+	a = a % b;		a %= b;
+	------------------------------------
+	*/
+#endif // COMPOUND_ASSIGNMENTS
+
+}
+
+/*
+-----------------------------
+Expression - это синтаксичская конструкция, состоящая из операндов и операторов.
+Операнды - это объекты (элеенты выражения), над которыми выполняется какое-то действие.
+Операторы - это объекты (элементы выражения), которые показывают, 
+			какие действия над операндами нужно выполнить.
+-----------------------------
+int
+-----------------------------
+*/
