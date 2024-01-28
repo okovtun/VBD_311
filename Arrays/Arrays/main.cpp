0e057a1259900ec1eaf4d5d93f54de56b89d8714
Clayman <Clayman@DESKTOP-DM8E77U>
2024-01-28 23:14:42 +0300

    Arrays & Bitwize operations

---
+++ Arrays/Arrays/main.cpp
@@ -0,0 +1,18 @@
+﻿//Arrays
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n] = { 3, 5, 8 };
+	//arr[1] = 1024;	//Обращение к элементу массива на запись
+	//arr[3] = 3072;	//Обращение к элементу массива на запись
+	//cout << arr[1] << endl;	//Обращение к элементу массива на чтение
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
