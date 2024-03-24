53016d94da9775109974a31f91d9d6694c0efe41
Clayman <Clayman@DESKTOP-DM8E77U>
2024-03-24 12:41:13 +0300

    Transfer parameters by value

---
+++ Pointers/TransferParameters/main.cpp
@@ -0,0 +1,22 @@
+//TransferParameters
+#include<iostream>
+using namespace std;
+
+void Exchange(int a, int b);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a = 2, b = 3;
+	cout << a << "\t" << b << endl;
+	Exchange(a, b);
+	cout << a << "\t" << b << endl;
+}
+
+void Exchange(int a, int b)
+{
+	int buffer = a;
+	a = b;
+	b = buffer;
+	cout << a << "\t" << b << endl;
+}
