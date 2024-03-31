321749687922500777b3cb249f93f482d8e34b14
Clayman <Clayman@DESKTOP-DM8E77U>
2024-03-31 17:46:54 +0300

    DynamicMemory day 2

--- Pointers/DynamicMemory/main.cpp
+++ Pointers/DynamicMemory/main.cpp
@@ -8,15 +8,26 @@ using std::endl;
 
 void FillRand(int arr[], const int n);
 void Print(const int arr[], const int n);
+void Print(int** arr, const int rows, const int cols);
+
+int* push_back(int arr[], int& n, const int value);
+int* push_front(int arr[], int& n, const int value);
+
+int* pop_back(int arr[], int& n);
+
+//#define DYNAMIC_MEMORY_1
+#define DYNAMIC_MEMORY_2
 
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef DYNAMIC_MEMORY_1
 	int n = 5;
 	cout << "Введите количество элементов: "; cin >> n;
 
 	//1) Объявление динамического массива:
 	int* arr = new int[n];
+	int* buffer = arr;
 
 	//2) Обращение к элементам динамического массива:
 
@@ -26,34 +37,56 @@ void main()
 	int value;
 	cout << "Введите добавляемое значение: "; cin >> value;
 
-	//1) Создаем буферный массив нужного размера:
-	int* buffer = new int[n + 1];
+	arr = push_back(arr, n, value);
+	Print(arr, n);
 
-	//2) Копируем все значения из исходного массива в буферный:
-	for (int i = 0; i < n; i++)
-	{
-		buffer[i] = arr[i];
-	}
-	
-	//3) Удаляем старый массив:
-	delete[] arr;
+	cout << "Введите добавляемое значение: "; cin >> value;
+	arr = push_front(arr, n, value);
+	Print(arr, n);
 
-	//4) Подменяем адрес старого массива адресом нового массива:
-	arr = buffer;
-
-	//5) Только после всего элтого можно добавить значение в конец массива:
-	arr[n] = value;	
-	
-	//6) после добавления элемента в конец массива, количество его элементов увеличивается на 1
-	n++;
-
+	arr = pop_back(arr, n);
 	Print(arr, n);
 
 	//3) Удаление динамического массива:
-	///delete[] buffer;
 	delete[] arr;
 
-	//Memory Leak
+	//Memory Leak  
+#endif // DYNAMIC_MEMORY_1
+
+	///////////////////////////////////////////////////////////////////////
+	///////		Объявление двумерного динамического массива			///////
+	///////////////////////////////////////////////////////////////////////
+
+	int rows;
+	int cols;
+	cout << "Введите количество строк: "; cin >> rows;
+	cout << "Введите количество элементов строки: "; cin >> cols;
+
+	//1) Создаем массив указателей:
+	int** arr = new int*[rows];
+	//2) Выделяем память под строки:
+	for (int i = 0; i < rows; i++)
+	{
+		arr[i] = new int[cols] {};
+	}
+
+	///////////////////////////////////////////////////////////////////////
+	///////		Использование двумерного динамического массива		///////
+	///////////////////////////////////////////////////////////////////////
+	
+	Print(arr, rows, cols);
+
+	///////////////////////////////////////////////////////////////////////
+	///////		  Удаление двумерного динамического массива			///////
+	///////////////////////////////////////////////////////////////////////
+
+	//1) Сначала удаляем строки двумерного массива:
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] arr[i];
+	}
+	//2) Удаляем массив указателей:
+	delete[] arr;
 }
 
 void FillRand(int arr[], const int n)
@@ -70,4 +103,68 @@ void Print(const int arr[], const int n)
 		cout << arr[i] << tab;		//обращение к элементам через оператор индексирования
 	}
 	cout << endl;
+}
+void Print(int** arr, const int rows, const int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
+}
+int* push_back(int arr[], int& n, int value)
+{
+	//1) Создаем буферный массив нужного размера:
+	int* buffer = new int[n + 1];
+
+	//2) Копируем все значения из исходного массива в буферный:
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+
+	//3) Удаляем старый массив:
+	delete[] arr;
+
+	//4) Подменяем адрес старого массива адресом нового массива:
+	arr = buffer;
+
+	//5) Только после всего элтого можно добавить значение в конец массива:
+	arr[n] = value;
+
+	//6) после добавления элемента в конец массива, количество его элементов увеличивается на 1
+	n++;
+
+	return arr;
+}
+int* push_front(int arr[], int& n, const int value)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i + 1] = arr[i];
+	}
+	delete[] arr;
+	buffer[0] = value;
+	n++;
+	return buffer;
+}
+
+int* pop_back(int arr[], int& n)
+{
+	/*int* buffer = new int[n - 1];
+	for (int i = 0; i < n - 1; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	delete[] arr;
+	n--;
+	return buffer;*/
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)buffer[i] = arr[i];
+	delete[] arr;
+	return buffer;
 }
---
+++ Pointers/Split/main.cpp
@@ -0,0 +1,69 @@
+﻿//Split
+#include<iostream>
+using namespace std;
+
+#define tab "\t"
+
+void FillRand(int arr[], const int n);
+void Print(const int arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 10;
+	int arr[n];
+	//.....
+	//.....
+	//.....
+	//delete[] arr;
+
+
+	FillRand(arr, n);
+	Print(arr, n);
+
+	//1) вычисляем размеры массивов:
+	int even_count = 0;
+	int odd_count = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] % 2 == 0)even_count++;
+		else odd_count++;
+	}
+	cout << "Количество четных элементов: " << even_count << endl;
+	cout << "Количество нечетных элементов: " << odd_count << endl;
+
+	//2) выделяем память под массивы:
+	int* even_arr = new int[even_count];
+	int* odd_arr = new int[odd_count];
+
+	//3) распределяем значения по разным массивам:
+	for (int i = 0, j = 0, k = 0; i < n; i++)
+	{
+		/*if (arr[i] % 2 == 0)even_arr[j++] = arr[i];
+		else			odd_arr[k++] = arr[i];*/
+		(arr[i] % 2 == 0 ? even_arr[j++] : odd_arr[k++]) = arr[i];
+	}
+
+	//4) выводим полученные массивы на экран:
+	cout << "Even arr: "; Print(even_arr, even_count);
+	cout << "Odd  arr: "; Print(odd_arr, odd_count);
+
+	delete[] odd_arr;
+	delete[] even_arr;
+}
+
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+}
+void Print(const int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
